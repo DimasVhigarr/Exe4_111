e6d08f0ec05fbb65b8e7913e2c0b58bd4b452c21
DimasVhigarr <dimasvhigar2002@gmail.com>
2022-12-07 15:49:51 +0700

    membuat class Node

--- Exe4_111/Program.cs
+++ Exe4_111/Program.cs
@@ -6,10 +6,14 @@ using System.Threading.Tasks;
 
 namespace Exe4_111
 {
-    internal class Program
+   class Node
     {
-        static void Main(string[] args)
+        public int info;
+        public Node next;
+        public Node(int i, Node n)
         {
+            info = i;
+            next = n;
         }
     }
 }
