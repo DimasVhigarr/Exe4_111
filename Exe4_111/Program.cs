67b981ae7ccfb8731f55336a09a7c5b53b08f1a1
DimasVhigarr <dimasvhigar2002@gmail.com>
2022-12-07 15:55:40 +0700

    menambahkan case 2

--- Exe4_111/Program.cs
+++ Exe4_111/Program.cs
@@ -79,6 +79,14 @@ namespace Exe4_111
                         int num = Convert.ToInt32(Console.ReadLine());
                         s.push(num);
                         break;
+                    case '2':
+                        if (s.empty())
+                        {
+                            Console.WriteLine("\nStack Empty");
+                            break;
+                        }
+                        s.pop();
+                        break;
                 }
             }
         }
